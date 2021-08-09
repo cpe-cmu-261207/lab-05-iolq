8a63709c623f70387551c5f8c9ba2cc419766738
iolq <wipawee_watt@cmu.ac.th>
2021-08-09 22:44:55 +0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useState } from 'react'
+import Footer from './footer';
+import Header from './header';
+import Todo from './todo';
 
 function App() {
 
+  const [todoList,setTodoList] = useState([]);
+  const [todo,setTodo] = useState("");
+
   const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
     /* check pressing enter key here */
+    setTodo(ev.key)
+    console.log(ev.key)
   }
 
   return (
@@ -12,23 +20,36 @@ function App() {
 
       {/* header section */}
       <div className='flex justify-center items-end space-x-2'>
+      <Header></Header>
+      {/* <div className='flex justify-center items-end space-x-2'>
         <span className='text-center italic my-2 text-2xl'>Minimal Todo List </span>
         <span className='text-gray-400 italic my-2 text-xl'>by ...</span>
       </div>
+        <span className='text-gray-400 italic my-2 text-xl'>by Wipawee Wattanatchariya 630612190</span>
+      </div> */}
 
       {/* todo section */}
       <div className='mx-auto max-w-4xl'>
+      <Todo></Todo>
+      {/* <div className='mx-auto max-w-4xl'> */}
 
         {/* task input and add button */}
         <div className='flex space-x-1'>
           <input className='border border-gray-400 w-full text-2xl'
             onKeyDown={onKeyDownCallback} ></input>
+        {/* <div className='flex space-x-1'>
+          <input className='border border-gray-400 w-full text-2xl' 
+            type="text"
+            onKeyDown={onKeyDownCallback}
+            id='input'></input>
           <button className='border border-gray-400 w-8 font-bold'>+</button>
         </div>
+        </div> */}
 
         {/* tasks section */}
         <div>
           {/* task example */}
+        {/* <div> */}
           {/* Please convert this into a task component */}
           <div
             className="flex justify-between h-8 items-center py-6 border-b"
@@ -54,10 +75,13 @@ function App() {
         </div>
       </div>
 
+        {/* </div> */}
+      {/* </div> */}
+
       {/* footer section */}
       <p className='text-center text-gray-400'> ... </p>
+      <Footer></Footer>
+      {/* <p className='text-center text-gray-400'> copyright © 2021  </p> */}
     </div>
   );
 }
-
-export default App;
